eb8df3c659979bd98d022aebcf1b84f3c51351a2
wuguai <chounong@126.com>
2019-12-10 10:14:54 +0800

    Add files via upload

---
+++ springboot-security-oauth2-demo1/src/main/java/com/sweetmart/SpringbootSecurityOauth2Demo1Application.java
@@ -0,0 +1,13 @@
+package com.sweetmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityOauth2Demo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityOauth2Demo1Application.class, args);
+	}
+
+}
